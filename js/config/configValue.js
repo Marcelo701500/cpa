4d01e783f00a227bd01096cc8cc7d3cb1b9fa087
[Marcelo de Oliveira] <[marcelo701500@gmail.com]>
2023-08-02 15:55:28 -0300

    Atualizaçao servidor dados

--- js/config/configValue.js
+++ js/config/configValue.js
@@ -1,5 +1,5 @@
 angular.module("controlePressaoArterial").value("config", {
-    //baseUrl: "https://restful-api-vercel-xi.vercel.app"
+    baseUrl: "https://restful-api-vercel-xi.vercel.app"
     //baseUrl: "http://localhost:3000"
-    baseUrl: "http://138.91.166.116:3000"
+    //baseUrl: "http://138.91.166.116:3000"
 })
