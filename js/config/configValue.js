a33010861098699665c17fb1d429a85fdcd9ddd7
[Marcelo de Oliveira] <[marcelo701500@gmail.com]>
2023-09-17 07:50:58 -0300

    Atualização do repositório

--- js/config/configValue.js
+++ js/config/configValue.js
@@ -1,6 +1,6 @@
 angular.module("controlePressaoArterial").value("config", {
-    baseUrl: "http://mdo.westus.cloudapp.azure.com:3000"
+    //baseUrl: "http://mdo.westus.cloudapp.azure.com:3000"
     //baseUrl: "https://restful-api-vercel-xi.vercel.app"
-    //baseUrl: "http://localhost:3000"
+    baseUrl: "http://localhost:3000"
     //baseUrl: "http://138.91.166.116:3000"
 })
